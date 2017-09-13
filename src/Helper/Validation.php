fa441b0e393a0ae19bc7e4cc4f1de348d074cbf9
Antoine KNOCKAERT <antoine@yper.fr>
2017-09-13 09:31:30 +0200

    [PRO] - Allow to order pro delivery from the SDK [API] - Handle scopes in API connector Updated examples

---
+++ src/Helper/Validation.php
@@ -0,0 +1,33 @@
+<?php
+# Copyright (c) 2013-2017, Yper.
+# All rights reserved.
+
+namespace Yper\SDK\Helper {
+
+    class Validation {
+
+        public static function validateParams($array, $validation) {
+
+            $finalParams = [];
+
+            foreach ($validation as $param => $conditions) {
+
+                if (isset($conditions["required"]) && $conditions["required"]) {
+                    if (!isset($array[$param]) || $array[$param] === null) {
+                        throw new \Exception("Missing parameter : " .  $param);
+                    }
+                }
+
+                $finalParams[$param] = $array[$param];
+
+                if (isset($conditions["required"]) && !$conditions["required"]
+                    && array_key_exists('default', $conditions) && !isset($array[$param])) {
+                    $finalParams[$param] = $conditions["default"];
+                }
+            }
+
+            return $finalParams;
+        }
+    }
+}
+
