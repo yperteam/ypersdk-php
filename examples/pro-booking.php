60a691d1d98589522ee5657b938847f9c7488ce6
Antoine KNOCKAERT <antoine@yper.fr>
2018-07-24 14:20:46 +0200

    Updated example with a prebook

--- examples/pro-booking.php
+++ examples/pro-booking.php
@@ -13,6 +13,8 @@ $applicationSecret = "YOUR_APP_SECRET";
 $pro_id = "PRO_ID";
 $pro_secret = "PRO_SECRET";
 
+
+
 try {
 
     // Instanciate API
@@ -25,34 +27,35 @@ try {
     print_r($proService->get_retailpoints()); // Get pro retailpoints
     print_r($proService->get_wallet()); // Get pro wallet
 
-//    print_r($proService->prebook([
-//        "delivery_address" => [
-//            "formatted_address" => "121 rue chanzy, 59260 Lille, France", // L'adresse du client en texte
-//            "additional_number" => null, // Complément sur le numéro de l'adresse : BIS|TER
-//            "additinal" => "Comment about the address", // Commentaire sur l'adresse du client
-//        ],
-//        "receiver" => [
-//            "firstname" => "John", // Prénom du client
-//            "lastname" => "Doe", // Nom du client
-//            "phone" => "+33612345678", // Téléphone du client
-//            "email" => "support@yper.fr" // Adresse email du client
-//        ],
-//        "retailpoint" => [
-//            "partner_id" => "00576" // Identifiant partenaire du magasin (votre identifint)
-//        ],
-//        "delivery_start" => "2018-01-28 16:00:00.000Z", // Heure de début de livraison
-//        "delivery_end" => "2018-01-28 17:00:08.000Z", // Defaults to "delivery_start" + 1 hour
-//        "order" => [
-//            "order_id" => "123456", // Numéro de commande
-//            "options" => ['climb'], // Options sur la commande
-//            "transport" => "car", // Taille de la commande
-//        ],
-//        "extra" => [
-//            "nb_items" => 3, // Nombre d'articles
-//            "price" => 49.5 // Prix de la commande (obligatoire pour le FRANCO)
-//        ],
-//        "comment" => "Comment displayed to the shopper" // Commentaire général à propos de la livraison
-//    ]));
+    print_r($proService->prebook([
+        "delivery_address" => [
+            "formatted_address" => "121 rue chanzy, 59260 Lille, France", // L'adresse du client en texte
+            "additional_number" => null, // Complément sur le numéro de l'adresse : BIS|TER
+            "additinal" => "Comment about the address", // Commentaire sur l'adresse du client
+        ],
+        "receiver" => [
+            "firstname" => "John", // Prénom du client
+            "lastname" => "Doe", // Nom du client
+            "phone" => "+33612345678", // Téléphone du client
+            "email" => "support@yper.fr" // Adresse email du client
+        ],
+        "retailpoint" => [
+            "id" => "00576" // Identifiant partenaire du magasin (votre identifint)
+        ],
+        "mission_template_id" => null, // Type de livraison yper (si non renseigné, prend celui par défaut)
+        "delivery_start" => "2018-01-28 16:00:00.000Z", // Heure de début de livraison
+        "delivery_end" => "2018-01-28 17:00:08.000Z", // Defaults to "delivery_start" + 1 hour
+        "order" => [
+            "order_id" => "123456", // Numéro de commande
+            "options" => ['climb'], // Options sur la commande // FACULTATIF
+            "transport" => "car", // Taille de la commande // FACULTATIF (Défaut : foot, moto, bike, car, break)
+        ],
+        "extra" => [
+            "nb_items" => 3, // Nombre d'articles
+            "price" => 49.5 // Prix de la commande
+        ],
+        "comment" => "Comment displayed to the shopper" // Commentaire général à propos de la livraison
+    ]));
 
 } catch(Exception $e) {
     echo "An error occured : " . $e->getMessage();
