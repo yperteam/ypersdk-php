51ff5678c5db471dc9cb06d14710baea49e8e2ae
Antoine KNOCKAERT <antoine@yper.fr>
2018-07-24 15:05:22 +0200

    Updated example with all methods

--- examples/pro-booking.php
+++ examples/pro-booking.php
@@ -13,7 +13,6 @@ $applicationSecret = "YOUR_APP_SECRET";
 $pro_id = "PRO_ID";
 $pro_secret = "PRO_SECRET";
 
-
 try {
 
     // Instanciate API
@@ -23,10 +22,11 @@ try {
 
     $proService = new Service\Pro($api, $pro_id);
 
-    print_r($proService->get_retailpoints()); // Get pro retailpoints
+    $rps = $proService->get_retailpoints();
+    print_r($rps); // Get pro retailpoints
     print_r($proService->get_wallet()); // Get pro wallet
 
-    print_r($proService->prebook([
+    $res = $proService->post_prebook([
         "delivery_address" => [
             "formatted_address" => "121 rue chanzy, 59260 Lille, France", // L'adresse du client en texte
             "additional_number" => null, // Complément sur le numéro de l'adresse : BIS|TER
@@ -39,22 +39,42 @@ try {
             "email" => "support@yper.fr" // Adresse email du client
         ],
         "retailpoint" => [
-            "id" => "00576" // Identifiant partenaire du magasin (votre identifint)
+            "id" => $rps[0]['_id'] // Identifiant partenaire du magasin (votre identifint)
         ],
         "mission_template_id" => null, // Type de livraison yper (si non renseigné, prend celui par défaut)
         "delivery_start" => "2018-01-28 16:00:00.000Z", // Heure de début de livraison
         "delivery_end" => "2018-01-28 17:00:08.000Z", // Defaults to "delivery_start" + 1 hour
         "order" => [
-            "order_id" => "123456", // Numéro de commande
+            "order_id" => "123456", // Numéro de commande // FACULTATIF : Si non saisi, nous en générons un par défaut
             "options" => ['climb'], // Options sur la commande // FACULTATIF
             "transport" => "car", // Taille de la commande // FACULTATIF (Défaut : foot, moto, bike, car, break)
         ],
         "extra" => [
-            "nb_items" => 3, // Nombre d'articles
             "price" => 49.5 // Prix de la commande
         ],
         "comment" => "Comment displayed to the shopper" // Commentaire général à propos de la livraison
-    ]));
+    ]);
+
+    print_r($res);
+
+    $prebook_id = $res['prebook_id'];
+
+    $res = $proService->post_validate_prebook($prebook_id);
+    $mission_id = $res['mission_id'];
+
+    print_r($res);
+
+    // Get mission informations
+    $mission = $proService->get_mission($mission_id);
+    print_r($mission);
+
+    // Return if mission is cancellable and the fee
+    $cancellable = $proService->get_cancel_mission($mission_id);
+    print_r($cancellable);
+
+    // Cancel the mission
+    $proService->post_cancel_mission($mission_id);
+    print_r($cancellable);
 
 } catch(Exception $e) {
     echo "An error occured : " . $e->getMessage();
