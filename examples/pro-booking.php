c3b9c78eaa6e2bb4d487aa729b003a8b85a7db01
AlexisGomes <alexis.gomes19@gmail.com>
2018-07-26 17:23:00 +0200

    Update pro-booking.php

--- examples/pro-booking.php
+++ examples/pro-booking.php
@@ -56,12 +56,11 @@ try {
     ]);
 
     print_r($res);
-
+    $prebook_id = $res['prebook_id'];
+    
     // Get mission template list
     $res = $proService->get_retailpoint_mission_templates($rps[0]["_id"]);
     print_r($res);
-
-    $prebook_id = $res['prebook_id'];
 
     $res = $proService->post_validate_prebook($prebook_id);
     $mission_id = $res['mission_id'];
