b5b0f2309bd26047a89ff40eab2ff93d10463622
Antoine Knockaert <antoine@yper.fr>
2019-04-09 20:46:30 +0200

    FIX: Fix URL path

--- src/Service/Pro.php
+++ src/Service/Pro.php
@@ -20,17 +20,17 @@ namespace Yper\SDK\Service {
         }
 
         public function get_retailpoints() {
-            $result = $this->get("pro/" . $this->pro_id . "/retailpoint");
+            $result = $this->get("/pro/" . $this->pro_id . "/retailpoint");
             return $result;
         }
 
         public function get_retailpoint_mission_templates($retailpoint_id) {
-            $result = $this->get("pro/" . $this->pro_id . "/retailpoint/" . $retailpoint_id . "/mission_template");
+            $result = $this->get("/pro/" . $this->pro_id . "/retailpoint/" . $retailpoint_id . "/mission_template");
             return $result;
         }
 
         public function get_wallet() {
-            $result = $this->get("pro/" . $this->pro_id . "/wallet");
+            $result = $this->get("/pro/" . $this->pro_id . "/wallet");
             return $result;
         }
 
@@ -38,27 +38,27 @@ namespace Yper\SDK\Service {
          * prebook a mission from a pro account
          */
         public function post_prebook($options = []) {
-            $result = $this->post("pro/" . $this->pro_id . "/prebook", $options);
+            $result = $this->post("/pro/" . $this->pro_id . "/prebook", $options);
             return $result;
         }
 
         public function post_validate_prebook($prebook_id) {
-            $result = $this->post("pro/" . $this->pro_id . "/prebook/" . $prebook_id . "/validate", null);
+            $result = $this->post("/pro/" . $this->pro_id . "/prebook/" . $prebook_id . "/validate", null);
             return $result;
         }
 
         public function get_mission($mission_id) {
-            $result = $this->get("pro/" . $this->pro_id . "/mission/" . $mission_id);
+            $result = $this->get("/pro/" . $this->pro_id . "/mission/" . $mission_id);
             return $result;
         }
 
         public function get_cancel_mission($mission_id) {
-            $result = $this->get("pro/" . $this->pro_id . "/mission/" . $mission_id . "/cancel");
+            $result = $this->get("/pro/" . $this->pro_id . "/mission/" . $mission_id . "/cancel");
             return $result;
         }
 
         public function post_cancel_mission($mission_id) {
-            $result = $this->post("pro/" . $this->pro_id . "/mission/" . $mission_id . "/cancel");
+            $result = $this->post("/pro/" . $this->pro_id . "/mission/" . $mission_id . "/cancel");
             return $result;
         }
 
