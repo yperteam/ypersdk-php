e60f62e9b5730bb5785502d2ff7908794f0e6926
Antoine Knockaert <antoine@yper.fr>
2019-05-08 15:49:41 +0200

    Add more endpoints

--- examples/pro-booking.php
+++ examples/pro-booking.php
@@ -10,22 +10,20 @@ use Yper\SDK\Api;
 use Yper\SDK\Service;
 
 // Informations about your application
-$applicationKey = "YOUR_APP_ID";
-$applicationSecret = "YOUR_APP_SECRET";
+$applicationKey = "7022265601";
+$applicationSecret = "m3ctRX8D8Q4zJV928uLaaUj8i2v6SH";
 
-$pro_id = "PRO_ID";
-$pro_secret = "PRO_SECRET";
-
+$pro_id = "5c08eaa1b2aa3ff829381032";
+$pro_secret = "36jgJm99d5JFwNU";
 try {
 
     // Instanciate API
     $api = new Api($applicationKey, $applicationSecret, [], 'beta'); // development | beta | production // defaults to : production
 
     $api->authenticate_pro_secret($pro_id, $pro_secret); // Authenticate with the pro_id and pro_secret ; Available in yper.pro backoffice
-
     $proService = new Service\Pro($api, $pro_id);
 
-    $address = "Maubeuge, France";
+    $address = "50 Rue du Commerce 59790 Ronchin";
 
     $rps = $proService->get_retailpoints();
     print_r($rps); // Get pro retailpoints
@@ -48,8 +46,8 @@ try {
                 "id" => $rps['result'][0]['_id'] // Identifiant partenaire du magasin (votre identifiant)
             ],
             "mission_template_id" => null, // Type de livraison yper (si non renseigné, prend celui par défaut)
-            "delivery_start" => "2019-01-28 16:00:00.000Z", // Heure de début de livraison
-            "delivery_end" => "2019-01-28 17:00:08.000Z", // Defaults to "delivery_start" + 1 hour
+            "delivery_start" => "2019-04-28 16:00:00.000Z", // Heure de début de livraison
+            "delivery_end" => "2019-04-28 17:00:08.000Z", // Defaults to "delivery_start" + 1 hour
             "order" => [
                 "order_id" => "123456", // Numéro de commande // FACULTATIF : Si non saisi, nous en générons un par défaut
                 "options" => ['climb'], // Options sur la commande // FACULTATIF
